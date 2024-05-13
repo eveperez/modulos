1353990ae37c8623578766ba833f24cb752db04c
evelin <evelinperez8163@gmail.com>
2024-05-13 08:54:23 -0600

    Main y modulo creado

---
+++ Main.py
@@ -0,0 +1,6 @@
+from Mimodulo import Sensor
+
+
+if __name__ == "__main__":
+    obj = Sensor()
+    print(obj.pin)
---
+++ Mimodulo.py
@@ -0,0 +1,9 @@
+class Sensor:
+    def __init__(self):
+        self.pin = 15
+
+    def inicializar(self,tipo):
+        self.tipo = tipo
+
+
+
